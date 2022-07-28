a63ff244d84115b098d30faafb6a7d3049bbec15
Elizabeth Klenczar <eklenczar@gmail.com>
2022-07-28 10:17:51 -0400

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,65 @@
-# your code goes here
+class Person
+    attr_reader :name, :hygiene
+    
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = value.clamp(0, 10)
+    end
+
+    def hygiene=(value)
+        @hygiene = value.clamp(0, 10)
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene=(@hygiene += 4)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness=(@happiness += 2)
+        self.hygiene=(@hygiene -= 3)
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else topic
+            "blah blah blah blah blah"
+        end
+    end
+end
+
